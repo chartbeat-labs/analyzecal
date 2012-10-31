7197527109d5bb7cf6f2c58dab8b18269d1c5af3
Allan Beaufour <allan@chartbeat.com>
2012-10-31 17:16:00 -0400

    better separation of logic

--- main.py
+++ main.py
@@ -107,90 +107,116 @@ class MainHandler(webapp.RequestHandler):
         self.response.out.write(template.render(variables))
 
 
+def _get_events(time_min, time_max):
+    """
+    Retrieve events from the Google API
+
+    @param time_min: datetime, from time
+    @param time_max: datetime, to time
+    @return: list(events)
+    """
+
+    http = decorator.http()
+    events = []
+    endpoint = service.events()
+    request = endpoint.list(
+        calendarId='primary',
+        singleEvents=True,
+        timeMin=time_min.isoformat('T') + 'Z',
+        timeMax=time_max.isoformat('T') + 'Z',
+        )
+    while request is not None:
+        response = request.execute(http=http)
+        events.extend(response.get('items', []))
+        request = endpoint.list_next(request, response)
+
+    return events
+
+
+def _generate_stats(time_min, time_max, events):
+    """
+    Generate stats from a list of events.
+
+    @param events: list(events), events to analyze
+    @param time_min: datetime, from time
+    @param time_max: datetime, to time
+    @return: list(events)
+    """
+
+    stats = {
+        'events': 0,
+        }
+    event_days = defaultdict(lambda: 0)
+    attendees = 0
+    total_duration_in_secs = 0
+    for event in events:
+        _ac = event['_ac'] = {}
+        if 'dateTime' not in event['start']:
+            # All-day events
+            continue
+
+        # TODO: Sort out declined and tentative events, if not
+        # event.organizer.self, find self in event.attendees
+        # list(dict)), 'email' = self and 'responseStatus' =
+        # 'accepted'
+        start = str_to_datetime(event['start']['dateTime'])
+        end = str_to_datetime(event['end']['dateTime'])
+
+        _ac['duration'] = end - start
+
+        if event['summary'] == 'Lunch':
+            # Don't count lunches as events
+            continue
+        if end.hour < WORK_DAY_START or start.hour > WORK_DAY_END:
+            # TODO: will miss multi-day events
+            continue
+        if start.weekday() > 4 or end.weekday() > 4:
+            # TODO: will exclude events ending or starting in
+            # weekends, but stretching into the week
+            continue
+
+        _ac['included'] = True
+        total_duration_in_secs += _ac['duration'].total_seconds()
+        event_days[start.weekday()] += 1
+        stats['events'] += 1
+        # if list not present, Default to 1 attendant (self)
+        attendees += len(event.get('attendees', ['1']))
+
+    # Calculate stats
+    stats['event_days'] = OrderedDict((v, event_days[k]) for (k, v) in WEEKDAY_TO_STR.iteritems())
+    stats['total_hours'] = total_duration_in_secs / 60 / 60
+    stats['working_days'] = num_working_days(time_min, time_max)
+    stats['working_hours'] = (WORK_DAY_END - WORK_DAY_START) * stats['working_days']
+    stats['percent_events'] = (stats['total_hours'] / stats['working_hours']) * 100
+    stats['avg_attendees'] = attendees / stats['events']
+    stats['avg_events_day'] = stats['events'] / stats['working_days']
+    stats['events_excluded'] = len(events) - stats['events']
+
+    return stats
+
+
 class AnalyzeHandler(webapp.RequestHandler):
     @decorator.oauth_required
     def get(self):
+        logging.info('Analyzing for: %s', users.get_current_user().nickname())
+
+        time_min = datetime.utcnow() - timedelta(weeks=NUM_WEEKS)
+        time_max = datetime.utcnow()
+
         try:
-            logging.info('Analyzing for: %s', users.get_current_user().nickname())
-
-            # Get calender events
-            timeMin = datetime.utcnow() - timedelta(weeks=NUM_WEEKS)
-            timeMax = datetime.utcnow()
-            http = decorator.http()
-            events = []
-            endpoint = service.events()
-            request = endpoint.list(
-                calendarId='primary',
-                singleEvents=True,
-                timeMin=timeMin.isoformat('T') + 'Z',
-                timeMax=timeMax.isoformat('T') + 'Z',
-                )
-            while request is not None:
-                response = request.execute(http=http)
-                events.extend(response.get('items', []))
-                request = endpoint.list_next(request, response)
-
-            # Iterate over events
-            stats = {
-                'events': 0,
-                }
-            event_days = defaultdict(lambda: 0)
-            attendees = 0
-            total_duration_in_secs = 0
-            for event in events:
-                _ac = event['_ac'] = {}
-                if 'dateTime' not in event['start']:
-                    # All-day events
-                    continue
-
-                # TODO: Sort out declined and tentative events, if not
-                # event.organizer.self, find self in event.attendees
-                # list(dict)), 'email' = self and 'responseStatus' =
-                # 'accepted'
-                start = str_to_datetime(event['start']['dateTime'])
-                end = str_to_datetime(event['end']['dateTime'])
-
-                _ac['duration'] = end - start
-
-                if event['summary'] == 'Lunch':
-                    # Don't count lunches as events
-                    continue
-                if end.hour < WORK_DAY_START or start.hour > WORK_DAY_END:
-                    # TODO: will miss multi-day events
-                    continue
-                if start.weekday() > 4 or end.weekday() > 4:
-                    # TODO: will exclude events ending or starting in
-                    # weekends, but stretching into the week
-                    continue
-
-                _ac['included'] = True
-                total_duration_in_secs += _ac['duration'].total_seconds()
-                event_days[start.weekday()] += 1
-                stats['events'] += 1
-                # if list not present, Default to 1 attendant (self)
-                attendees += len(event.get('attendees', ['1']))
-
-            # Calculate stats
-            stats['event_days'] = OrderedDict((v, event_days[k]) for (k, v) in WEEKDAY_TO_STR.iteritems())
-            stats['total_hours'] = total_duration_in_secs / 60 / 60
-            stats['working_days'] = num_working_days(timeMin, timeMax)
-            stats['working_hours'] = (WORK_DAY_END - WORK_DAY_START) * stats['working_days']
-            stats['percent_events'] = (stats['total_hours'] / stats['working_hours']) * 100
-            stats['avg_attendees'] = attendees / stats['events']
-            stats['avg_events_day'] = stats['events'] / stats['working_days']
-            stats['events_excluded'] = len(events) - stats['events']
-
-            # Render
-            data = {
-                'events': events,
-                'stats': stats,
-                'pformat': pformat,
-                }
-            template = jinja_environment.get_template('analyze.html')
-            self.response.out.write(template.render(data))
-
+            events = _get_events(time_min, time_max)
         except AccessTokenRefreshError:
-            self.redirect('/')
+            return self.redirect('/')
+
+        stats = _generate_stats(time_min, time_max, events)
+
+        data = {
+            'events': events,
+            'stats': stats,
+            'pformat': pformat,
+            }
+        template = jinja_environment.get_template('analyze.html')
+        self.response.out.write(template.render(data))
 
 
 def main():
