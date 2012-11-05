045d5687db2e53be1aa4afd05d54e44d7a5cf2f2
Allan Beaufour <allan@beaufour.dk>
2012-11-05 12:05:00 -0500

    add logout link to homepage

--- handlers/index.py
+++ handlers/index.py
@@ -1,3 +1,4 @@
+from google.appengine.api import users
 from google.appengine.ext import webapp
 
 from handlers.env import decorator
@@ -16,6 +17,7 @@ class IndexHandler(webapp.RequestHandler):
             'has_credentials': decorator.has_credentials(),
             'title': 'Welcome',
             'page': 'home',
+            'logout_url': users.create_logout_url('/'),
             }
         template = template_engine.get_template('index.html')
         self.response.out.write(template.render(data))
